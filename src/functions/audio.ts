640cd29ba829020b6a5d548a02aaae2707dbeb46
akia <ch.royer15@gmail.com>
2024-01-06 22:07:46 +0100

    test

--- src/functions/audio.ts
+++ src/functions/audio.ts
@@ -1,7 +1,22 @@
 export const playAudio =  (name:string, volume:number) : void => {
-    const audio = new Audio(require(`@/assets/${name}.mp3`));
-    audio.volume = volume;
-    audio.play()
+    // const audio = new Audio(require(`@/assets/${name}.mp3`));
+    // audio.volume = volume;
+    // audio.play()
+    const audioCtx = new AudioContext();
+    const gainNode = audioCtx.createGain();
+
+    import(`@/assets/${name}.mp3`)
+        .then((module) => {
+            const audio = new Audio(module.default);
+            const source = audioCtx.createMediaElementSource(audio);
+            source.connect(gainNode);
+            gainNode.connect(audioCtx.destination);
+            gainNode.gain.value = volume;
+            audio.play();
+        })
+        .catch((error) => {
+            console.error('Erreur de chargement audio :', error);
+        });
 };
 
 export const chargeAudio = () => {
